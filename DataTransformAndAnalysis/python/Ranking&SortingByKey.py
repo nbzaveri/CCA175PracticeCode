13e6d3040550128b1527da7e9c69e5b05db9ab54
Nishit Zaveri <zaveri.nb@gmail.com>
2017-01-19 07:02:41 +0530

    Ranking & Sorting By Key

--- DataTransformAndAnalysis/python/Ranking&SortingByKey.py
+++ DataTransformAndAnalysis/python/Ranking&SortingByKey.py
@@ -9,3 +9,27 @@ productMap = products.map(lambda rec: (rec.split(",")[1], rec))
 
 #groupByKey gives values in iterable type, converting it to list so that we can see the values
 productGroup = productMap.groupByKey().map(lambda rec: (rec[0], list(rec[1])))
+
+#flatmap to get all list value available
+#sorted will sort the list
+for i in productGroup.flatMap(lambda rec: sorted(rec[1])).collect():	print i
+
+productGroup = productMap.groupByKey()
+#sort data based on certain key (here product price is key)
+#this provide data sorted by product price per categoty
+for i in productGroup.flatMap(lambda rec: sorted(rec[1], key=lambda k: k.split(",")[4])).collect():	print i
+#without float converstion it does sorting on string price value
+for i in productGroup.flatMap(lambda rec: sorted(rec[1], key=lambda k: float(k.split(",")[4]))).collect():	print i
+
+#descending order
+for i in productGroup.flatMap(lambda rec: sorted(rec[1], key=lambda k: float(k.split(",")[4]),reverse=True)).collect():	print i
+
+
+##Get TopN product by price in each category
+def TopNData(rec, topN):
+	x = [ ] #crete a blank list
+	x = list(sorted(rec[1],key=lambda k: float(k.split(",")[4]),reverse=True)) #apply sorted function
+	import itertools
+	return (y for y in list(itertools.islice(x,0,topN))) #get topN data
+	
+for i in productGroup.flatMap(lambda rec: TopNData(rec, 10)).collect():	print i
