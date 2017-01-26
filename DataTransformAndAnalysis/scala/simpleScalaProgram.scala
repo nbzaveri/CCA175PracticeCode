4cbd23748ce69a45e38e51e6387d635c545918a6
Nishit Zaveri <zaveri.nb@gmail.com>
2017-01-26 08:18:42 +0530

    simple scala app

---
+++ DataTransformAndAnalysis/scala/simpleScalaProgram.scala
@@ -0,0 +1,14 @@
+/**
+* sbt package
+* spark-submit --class "SimpleApp" --master local /home/cloudera/scala/target/scala-2.10/simple-project_2.10-1.0.jar
+**/
+import org.apache.spark.SparkContext, org.apache.spark.SparkConf
+object SimpleApp{
+	def main(args: Array[String]){
+		val conf=new SparkConf().setAppName("Simple scala program")
+		val sc = new SparkContext(conf)
+
+		val dptsRdd = sc.textFile("/user/cloudera/tableData/departments")
+		dptsRdd.saveAsTextFile("/user/cloudera/scala")	
+	}
+}
